683bc186cd2bd55489a1d4882e62119db15d0ece
pohorg43 <118085085+pohorg43@users.noreply.github.com>
2026-06-04 17:25:20 +0300

    Change VENDOR_EDIT directive to include guard

--- include/uapi/media/cam_defs.h
+++ include/uapi/media/cam_defs.h
@@ -675,7 +675,7 @@ struct cam_dump_req_cmd {
 	uint32_t       reserved;
 };
 
-#ifdef VENDOR_EDIT
+#ifndef VENDOR_EDIT
 #define CAM_OEM_COMMON_OPCODE_BASE                  0x8000
 
 #define CAM_OEM_RW_SIZE_MAX        128
